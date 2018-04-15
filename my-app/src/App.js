0a238dedabd46a51519cadcea95c6b5a6049c5ee
Salina <salina.sallu@outlook.com>
2018-04-15 11:20:50 +0545

    material-ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import User from './User';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 
+
+import User from './User'
+import { Drawer } from 'material-ui';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      "open": false,
+    };
+  }
+  handleToggle = () => this.setState({open: !this.state.open});
+  
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-            <User  firstName={firstName} lastName={lastName} />
-           
-        </p>
-      </div>
+      <MuiThemeProvider>
+      <AppBar
+      title="APPBAR"
+      iconClassNameRight="muidocs-icon-navigation-expand-more" 
+      onLeftIconButtonClick={this.handleToggle}
+            />
+
+      <Drawer
+        open={this.state.open}
+        onRequestChange={(open)=> this.setState({open})}>
+        <AppBar title = "It's Open!"
+        onLeftIconButtonClick={this.handleToggle}
+        />
+      </Drawer>
+        
+        
+
+
+        <User firstName = {firstName} lastName= {lastName} />
+
+     
+    </MuiThemeProvider>
+      
     );
   }
 }
 
-const firstName="Salina";
-const lastName="Maharjan";
+const firstName = "Salina";
+const lastName = " Maharjan";
 
 export default App;
